f168505311523be8830a1aae115fb0a25d337ef3
parkchanjae <ekzm2302@naver.com>
2022-09-21 14:12:16 +0900

    carousel script finish
    미니언즈2 reviews 캐러셀 적용

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded", function() {
 	const POPUP = document.querySelector("#popup");
 	const BODY = document.querySelector("body");
 	const CLOSE = document.querySelector("#close");
+	const POSTS = document.querySelector("#posts");
 	function showAfterLoad() {  // 자바 메소드처럼 선언!
 		POPUP.style.display = "block";
 		BODY.style.overflow = "hidden";
@@ -12,5 +13,22 @@ window.addEventListener("DOMContentLoaded", function() {
 		POPUP.style.display = "none";
 		BODY.style.overflow = "auto";
 	})
-
+	const NEXT = document.querySelector(".next");
+	let postIndex = 0;
+	NEXT.addEventListener("click", function(){
+		postIndex ++;
+		// console.log("인덱스 : " +postIndex);
+		if(postIndex >= 3) postIndex = 0;
+		let coords = -920 * postIndex;
+		POSTS.style.marginLeft = coords+"px";
+	})
+	
+	const PREV = document.querySelector(".prev");
+	PREV.addEventListener("click", function(){
+		postIndex --;
+		// console.log("인덱스 : " +postIndex);
+		if(postIndex < 0) postIndex = 2;
+		let coords = -920 * postIndex;
+		POSTS.style.marginLeft = coords+"px";
+	})
 })
